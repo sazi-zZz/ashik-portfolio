40bfe0cada64cfb6ec172213809afbd354ceceb6
lesgo-Beyond <sazedurrahman707@gmail.com>
2026-06-30 03:31:53 +0600

    complete

---
+++ script.js
@@ -0,0 +1,12 @@
+function overlay(imgElement) {
+    const img_overlay = document.getElementById("img_overlay");
+    const overlay_image = document.querySelector("#overlay-image img");
+
+    img_overlay.style.display = "flex";
+    overlay_image.src = imgElement.src;
+}
+function overlay_close(close_btn) {
+    const img_overlay = document.getElementById("img_overlay");
+
+    img_overlay.style.display = "none";
+}
